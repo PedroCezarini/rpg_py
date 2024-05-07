41dc412c3b77974f91d17f53756886ec2b68b402
Pedro Cezarini <135235828+PedroCezarini@users.noreply.github.com>
2024-05-07 10:40:45 -0300

    Update personagem.py
    mudando os personagens de subclasses para funções.

--- personagem.py
+++ personagem.py
@@ -1,33 +1,32 @@
 class Personagem:
-    def __init__(self, name, life, attack_type):
+    def __init__(self, name, life, mana):
         self.name = name
         self.life = life
-        self.attack_type = attack_type
-
-def getAttack(self, n):
-        if 0 <= n < len(self.attack_type):
-            return f"Ataque {n + 1} - {self.attack_type[n]}, custo de mana."
-        else:
-            return "Ataque inválido."
-
-class Mago(Personagem):
-    def __init__(self, name):
-        super().__init__(name, 70, ["Bola de fogo", "Espinho de gelo", "Missel arcano"])
-
-class Guerreiro(Personagem):
-    def __init__(self, name):
-        super().__init__(name, 100, ["Golpe giratório", "Golpe perfurante", "Fura bucho"])
-
-class Ladino(Personagem):
-    def __init__(self, name):
-        super().__init__(name, 80, ["Ataque das sombras", "Golpe fantasma", "Lâmina das trevas"])
+        self.mana = mana
+      
+        # Método para mudar os atributos
         
+    def setMago(self,new_name="Magnus,o Mago",new_life=70,new_mana=100):
+        self.name=new_name
+        self.life=new_life
+        self.mana=new_mana
+        self.a1="Bola de fogo"
+        self.a2="Espinho de gelo"
+        self.a3="Missel arcano"
 
-class Inimigo(Personagem):
-    def __init__(self, name, life): # Deve ser passado os pontos de ataque do inimigo?
-        super().__init__(name, life, [])  # Chama o construtor da superclasse Personagem
-        
 
-# Exemplo de uso:
-# meu_personagem = Personagem("Herói", 100, [])
-# inimigo = Inimigo("Zequinha Dragão", 50)
+    def setGuerreiro(self,new_name="Absalom,o Barbaro",new_life=100,new_mana=50):
+        self.name=new_name
+        self.life=new_life
+        self.mana=new_mana
+        self.a1="Golpe giratório"
+        self.a2="Golpe perfurante"
+        self.a3="Rasga bucho"
+
+    def setLadino(self,new_name="Febror, o Ladino",new_life=80,new_mana=60):
+        self.name=new_name
+        self.life=new_life
+        self.mana=new_mana
+        self.a1="Ataque das sombras"
+        self.a2="Golpe fantasma"
+        self.a3="Lâmina das trevas"
